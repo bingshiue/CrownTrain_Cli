867ead3c16adb8774732c5e555555088cc7b3feb
bingshiue <bingshiue@gmail.com>
2015-01-27 22:03:37 +0800

    Create Project
    Signed-off-by: bingshiue <bingshiue@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ *
+ */
+#include "main.h"
+
+int main(int argc,char** argv){
+
+	//
+	using namespace std;
+	cout<< "To be Implemented " << endl;
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
---
+++ main.h
@@ -0,0 +1,17 @@
+/**
+ * @file main.h
+ *
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include <iostream>
+
+
+#endif /* MAIN_H_ */
